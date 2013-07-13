5aadc5f2b15a9f80017d220c1c6c22b3e33a46ce
James Sha <jamessha78@berkeley.edu>
2013-07-13 07:39:15 -0700

    http api added

--- Backend/db.py
+++ Backend/db.py
@@ -1,6 +1,8 @@
 import time
 from pymongo import MongoClient
+from flask import Flask, request
 from haversine import multipleUserThresh
+app = Flask(__name__)
 
 AEPI = (37.867062, -122.251914, 0)
 TOPDOG = (37.868315, -122.257472, 0)
@@ -16,21 +18,24 @@ def encode_group(group):
     return {'_type': 'Group',
             'group_id': group.group_id,
             'phone_name_map': group.phone_name_map,
-            'phone_loc_map': group.phone_loc_map}
+            'phone_loc_map': group.phone_loc_map,
+            'phone_dnd_map': group.phone_dnd_map}
 
 
 def decode_group(doc):
     assert doc['_type'] == 'Group'
     return Group(doc['group_id'],
                  doc['phone_name_map'],
-                 doc['phone_loc_map'])
+                 doc['phone_loc_map'],
+                 doc['phone_dnd_map'])
 
 
 class Group():
-    def __init__(self, group_id, phone_name_map, phone_loc_map):
+    def __init__(self, group_id, phone_name_map, phone_loc_map, phone_dnd_map):
         self.group_id = group_id
         self.phone_name_map = phone_name_map
         self.phone_loc_map = phone_loc_map
+        self.phone_dnd_map = phone_dnd_map
 
 
 class Backend():
@@ -58,36 +63,38 @@ class Backend():
         filtered_groups = {}
         group = self._get_group(group_id)
         for phone in group.phone_loc_map.keys():
-            if phone != my_id and group.phone_loc_map[phone] is not None:
+            if phone != my_id and group.phone_loc_map[phone] is not None and group.phone_dnd_map[phone] == 1:
                 filtered_groups[phone] = group.phone_loc_map[phone]
         return filtered_groups
 
-    def add_user(self, group_id, phone_id, phone_name, debug=False):
-        try:
-            data = self._get_group(group_id)
-        except:
-            data = Group(group_id, dict(), dict())
-            doc = {'ID': group_id,
-                   'Data': encode_group(data)}
-            self.groups.insert(doc)
-        
-        data.phone_name_map[phone_id] = phone_name
-        data.phone_loc_map[phone_id] = None
-        self._update_group(group_id, data)
-        # Ensures a user can only be part of one group
-        if phone_id in self.users_group_map.keys():
-            self.remove_user(self.users_group_map[phone_id], phone_id)
-        self.users_group_map[phone_id] = group_id
-        return True
-
-    def remove_user(self, group_id, phone_id):
+    def _remove_user(self, group_id, phone_id):
         group = self._get_group(group_id)
         group.phone_name_map.pop(phone_id, None)
         group.phone_loc_map.pop(phone_id, None)
         self._update_group(group_id, group)
         self.users_group_map.pop(phone_id, None)
 
-    def update_location(self, group_id, phone_id, location, debug=False):
+    def add_user(self, group_id, phone_id, phone_name, toggle_state):
+        if phone_id in self.users_group_map.keys():
+            self._remove_user(self.users_group_map[phone_id], phone_id)
+        self.users_group_map[phone_id] = group_id
+        
+        try:
+            data = self._get_group(group_id)
+        except:
+            data = Group(group_id, dict(), dict(), dict())
+            doc = {'ID': group_id,
+                   'Data': encode_group(data)}
+            self.groups.insert(doc)
+        
+        data.phone_name_map[phone_id] = phone_name
+        data.phone_loc_map[phone_id] = None
+        data.phone_dnd_map[phone_id] = toggle_state
+        self._update_group(group_id, data)
+        # Ensures a user can only be part of one group
+        return True
+
+    def update_location(self, group_id, phone_id, location):
         group = self._get_group(group_id)
         my_name = group.phone_name_map[phone_id]
         print '============UPDATING {0} LOCATION============='.format(my_name)
@@ -125,7 +132,7 @@ class Backend():
         return offending_names
 
 
-def main():
+def test():
     backend = Backend()
 
     phone_id_1 = 'Alpha'
@@ -141,9 +148,9 @@ def main():
     group_id_3 = 'Group'
     
     # Add three test users
-    backend.add_user(group_id_1, phone_id_1, phone_name_1)
-    backend.add_user(group_id_2, phone_id_2, phone_name_2)
-    backend.add_user(group_id_3, phone_id_3, phone_name_3)
+    backend.add_user(group_id_1, phone_id_1, phone_name_1, True)
+    backend.add_user(group_id_2, phone_id_2, phone_name_2, True)
+    backend.add_user(group_id_3, phone_id_3, phone_name_3, True)
     
     # Test location updates
     # First update shouldn't yelt any notices
@@ -167,8 +174,8 @@ def main():
     time.sleep(time_to_wait)
 
     # Test removal of someone from the group
-    backend.remove_user(group_id_1, phone_id_1)
-    backend.add_user('Wrong', phone_id_3, phone_name_3, debug=True)
+    backend.add_user('Wrong', phone_id_1, phone_name_1, True)
+    backend.add_user('Wrong', phone_id_3, phone_name_3, True)
     # Should be no notice since phone 1 is not in a group and phone 3
     # in a different group
     backend.update_location(group_id_2, phone_id_2, AEPI)
@@ -180,9 +187,9 @@ def main():
     time.sleep(time_to_wait)
     
     # Test updates after waiting past WAIT
-    backend.add_user(group_id_1, phone_id_1, phone_name_1)
+    backend.add_user(group_id_1, phone_id_1, phone_name_1, True)
     #print backend._get_group(group_id_1).phone_loc_map
-    backend.add_user(group_id_3, phone_id_3, phone_name_3)
+    backend.add_user(group_id_3, phone_id_3, phone_name_3, True)
     # First update should notify pairs (1, 2) since 2 already there
     backend.update_location(group_id_1, phone_id_1, AEPI)
     # Second update should not notify (no change)
@@ -190,5 +197,28 @@ def main():
     # Third update should notify pairs (1, 3), (2, 3)
     backend.update_location(group_id_3, phone_id_3, HASTE)
 
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    if request.method == 'POST':
+        group_id = request.form['group_id']
+        phone_id = request.form['phone_id']
+        phone_name = request.form['phone_name']
+        dnd = int(request.form['dnd'])
+        backend.add_user(group_id, phone_id, phone_name, dnd)
+        return "Done"
+
+@app.route('/update_location', methods=['POST'])
+def update_location():
+    if request.method == 'POST':
+        group_id = request.form['group_id']
+        phone_id = request.form['phone_id']
+        lon = float(request.form['lon'])
+        lat = float(request.form['lat'])
+        acc = float(request.form['acc'])
+        location = (lon, lat, acc)
+        backend.update_location(group_id, phone_id, location)
+        return "Done"
+
+backend = Backend()
 if __name__ == '__main__':
-    main()
+    app.run()
