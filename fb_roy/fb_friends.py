2196e6e4e8d43f9e0a0536c886fb0e66131b77b7
Roy Iacob <roy@Empty-Macbook-Pro.local>
2013-07-12 22:04:59 -0700

    facebook api: get friends, push_notification

--- fb_roy/fb_friends.py
+++
@@ -1,8 +0,0 @@
-import facebook as f
-
-token = 'CAAHHJaqayWIBAH3lqJ6PbpLTwPHED34fxFQ3p2iZBnn6vf7ESjQ0Q7pGBZBld1nM2ZCh9ytdjndBbYe4dZCdGj6vM9tHPLK6PNkXAvxZBFnH9EQlDb6EpmmrTcEJqUq71VTie8PlaFHNEj2yzj0rl1WwgZCfd43fIZD'
-def get_friends(token):
-    graph = f.GraphAPI(token)
-    friends = gr.get_connections('me','friends')
-    return friends
-
